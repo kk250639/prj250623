7bab1676d32b18964cda0e907d55b64c2ce09783
kk250639 <chesyeo@gmail.com>
2025-06-23 18:00:35 +0900

    table, entity, repository, service

--- src/main/java/com/example/prj250623/board/controller/BoardController.java
+++ src/main/java/com/example/prj250623/board/controller/BoardController.java
@@ -1,26 +1,31 @@
 package com.example.prj250623.board.controller;
 
 import com.example.prj250623.board.dto.BoardForm;
+import com.example.prj250623.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("board")
-public class boardController {
+public class BoardController {
+
+    private final BoardService boardService;
 
     @GetMapping("write")
-    public String write() {
-        return "/board/write";
+    public String writeForm() {
+
+        return "board/write";
     }
 
     @PostMapping("write")
     public String writePost(BoardForm data) {
-        System.out.println("data = " + data);
-        return "/board/write";
+
+        boardService.add(data);
+
+        return "board/write";
     }
 }
