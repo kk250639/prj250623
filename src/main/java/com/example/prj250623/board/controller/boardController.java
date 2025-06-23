61050b040672750cc1ae127838d025557000030e
kk250639 <chesyeo@gmail.com>
2025-06-23 14:55:50 +0900

    쓰기 복습2

--- src/main/java/com/example/prj250623/board/controller/boardController.java
+++ src/main/java/com/example/prj250623/board/controller/boardController.java
@@ -1,5 +1,25 @@
 package com.example.prj250623.board.controller;
 
+import com.example.prj250623.board.dto.BoardForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("board")
 public class boardController {
+
+    @GetMapping("write")
+    public String write() {
+        return "/board/write";
+    }
+
+    @PostMapping("write")
+    public String writePost() {
+        return "/board/write";
+    }
 }
