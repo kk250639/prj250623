bdce6ac59a80c2e6dacd39431db7e202a24c2b46
kk250639 <chesyeo@gmail.com>
2025-06-23 16:51:41 +0900

    reset

--- src/main/java/com/example/prj250623/board/controller/boardController.java
+++ src/main/java/com/example/prj250623/board/controller/boardController.java
@@ -1,26 +1,5 @@
 package com.example.prj250623.board.controller;
 
-import com.example.prj250623.board.dto.BoardForm;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("board")
 public class boardController {
 
-    @GetMapping("write")
-    public String write() {
-        return "/board/write";
-    }
-
-    @PostMapping("write")
-    public String writePost(BoardForm data) {
-        System.out.println("data = " + data);
-
-        return "/board/write";
-    }
 }
