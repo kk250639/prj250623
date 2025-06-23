008b8bc463595cdd2ccecb2b3f6b0bf486667300
kk250639 <chesyeo@gmail.com>
2025-06-23 09:03:12 +0900

    first commit

---
+++ src/test/java/com/example/prj250623/Prj250623ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj250623;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj250623ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
